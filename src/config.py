164c884c666a0b8e5eef49c1a7998add62214134
arti <loscoarti@gmail.com>
2023-11-03 23:01:10 +0100

    upgrade config .env

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
@@ -15,4 +15,8 @@ class Settings(BaseSettings):
             f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         )
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
