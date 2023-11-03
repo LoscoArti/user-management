ad7f10962bdd79acd8a15aaf5b86a65ff828714b
arti <loscoarti@gmail.com>
2023-11-03 22:31:53 +0100

    add autocommit and autoflash by default

--- src/db/database.py
+++ src/db/database.py
@@ -7,7 +7,13 @@ SQLALCHEMY_DATABASE_URL = settings.async_database_url()
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
-async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+async_session = sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+    autocommit=False,
+    autoflush=False,
+)
 
 
 async def get_session() -> AsyncSession:
