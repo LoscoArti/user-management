3e754b0b47b218a0ed481f9f57484c157e7d3b6d
arti <loscoarti@gmail.com>
2023-11-03 22:16:06 +0100

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
-from typing import Union
-
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="User Management service",
+    description="Main service is responsible for registration \
+                and authentication users",
+)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/healthcheck")
+async def health_check():
+    return {"response": "Ok"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
