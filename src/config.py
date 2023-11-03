0b155f60e482566a9c1df88829984c657d3bae64
arti <loscoarti@gmail.com>
2023-11-03 22:55:38 +0100

    remove DB_SERVER

--- src/config.py
+++ src/config.py
@@ -7,13 +7,12 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: str
     DB_PORT: int
-    DB_SERVER: str
 
     @property
     def async_database_url(self) -> str:
         return (
             f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}"
-            f"@{self.DB_SERVER}:{self.DB_PORT}/{self.DB_NAME}"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         )
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
