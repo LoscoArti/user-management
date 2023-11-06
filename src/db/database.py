e23c5308376d433287498f36dedd22bbe0cfc771
arti <loscoarti@gmail.com>
2023-11-06 22:37:37 +0100

    Add async_sessionmaker and DeclarativeBase to database.py, update models.py with new imports and annotations

--- src/db/database.py
+++ src/db/database.py
@@ -1,5 +1,5 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
@@ -7,7 +7,7 @@ SQLALCHEMY_DATABASE_URL = settings.async_database_url()
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
-async_session = sessionmaker(
+async_session = async_sessionmaker(
     engine,
     expire_on_commit=False,
     class_=AsyncSession,
@@ -19,3 +19,7 @@ async_session = sessionmaker(
 async def get_session() -> AsyncSession:
     async with async_session() as session:
         yield session
+
+
+class Base(DeclarativeBase):
+    pass
--- src/user/models.py
+++ src/user/models.py
@@ -1,12 +1,26 @@
-from sqlalchemy import Enum, String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+import datetime
+import enum
+from typing import Annotated
+
+from sqlalchemy import Enum, ForeignKey, Text, text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from db.database import Base
+
+intpk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[
+    datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))
+]
+modified_at = Annotated[
+    datetime.datetime,
+    mapped_column(
+        server_default=text("TIMEZONE('utc', now())"),
+        onupdate=datetime.datetime.utcnow,
+    ),
+]
 
 
-class Base(DeclarativeBase):
-    pass
-
-
-class Role(Enum):
+class Role(enum.Enum):
     USER = "USER"
     ADMIN = "ADMIN"
     MODERATOR = "MODERATOR"
@@ -15,20 +29,20 @@ class Role(Enum):
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    id: Mapped[intpk]
+    name: Mapped[str] = mapped_column(Text(30), nullable=False)
     surname: Mapped[str]
     username: Mapped[str]
     phone_number: Mapped[str]
     email: Mapped[str]
     image_s3_path: Mapped[str]
-    is_blocked: Mapped[bool]
-    created_at: Mapped[str]
-    modified_at: Mapped[str]
+    is_blocked: Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[created_at] = mapped_column(default=created_at)
+    modified_at: Mapped[modified_at]
 
-    role: Mapped["Role"] = mapped_column(Enum(Role), nullable=False)
+    role: Mapped[Role] = mapped_column(Enum(Role), nullable=False, default=Role.USER)
 
-    groups: Mapped["Group"] = relationship("Group", back_populates="user")
+    group_id: Mapped[int] = relationship(ForeignKey("groups.id"))
 
     def __repr__(self) -> str:
         return f"<User(id={self.id}, name={self.name}"
@@ -37,6 +51,6 @@ class User(Base):
 class Group(Base):
     __tablename__ = "groups"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(50), nullable=False)
-    created_at: Mapped[str]
+    id: Mapped[intpk]
+    name: Mapped[str] = mapped_column(Text(50), nullable=False)
+    created_at: Mapped[created_at] = mapped_column(default=created_at)
