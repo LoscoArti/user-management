3db3116a9b4d5c157a02a40d2598474191266218
arti <loscoarti@gmail.com>
2023-11-08 18:09:49 +0100

    Refactor User and Group models

--- src/user/models.py
+++ src/user/models.py
@@ -31,22 +31,19 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        primary_key=True,
-        default=uuid.uuid4,
-        nullable=False,
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
     name: Mapped[str] = mapped_column(Text)
     surname: Mapped[str] = mapped_column(Text, nullable=True)
     username: Mapped[str] = mapped_column(Text, nullable=True)
     phone_number: Mapped[str] = mapped_column(Text, nullable=True)
-    email: Mapped[str] = mapped_column(Text, nullable=True)
+    email: Mapped[str] = mapped_column(Text)
     image_s3_path: Mapped[str] = mapped_column(Text, nullable=True)
     is_blocked: Mapped[bool] = mapped_column(Boolean, default=False)
     created_at: Mapped[created_timestamp]
     modified_at: Mapped[modified_timestamp]
 
-    role: Mapped[Role] = mapped_column(Enum(Role), nullable=False, default=Role.USER)
+    role: Mapped[Role] = mapped_column(Enum(Role), default=Role.USER)
 
     group_id: Mapped[int] = mapped_column(ForeignKey("groups.id"), nullable=True)
 
@@ -60,7 +57,7 @@ class Group(Base):
     __tablename__ = "groups"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(Text, nullable=False)
+    name: Mapped[str] = mapped_column(Text)
     created_at: Mapped[created_timestamp]
 
     users: Mapped[list[User]] = relationship("User", back_populates="group")
