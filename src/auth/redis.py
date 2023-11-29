2836641c2a401375e807faaed8331bf644016a85
arti <loscoarti@gmail.com>
2023-11-29 12:41:11 +0100

    Refactor Redis client and add async context manager

--- src/auth/redis.py
+++ src/auth/redis.py
@@ -1,35 +1,31 @@
+from contextlib import asynccontextmanager
+
 from redis.asyncio import ConnectionPool, Redis
 
 from src.config import settings
 
-redis_client = None
 
-
+@asynccontextmanager
 async def get_redis_client() -> Redis:
-    global redis_client
-    if redis_client is None:
-        pool = ConnectionPool.from_url(
-            f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/{settings.REDIS_DB}",
-            encoding="utf-8",
-        )
-        redis_client = Redis(connection_pool=pool)
-    return redis_client
-
-
-async def close_redis_client():
-    global redis_client
-    if redis_client:
-        await redis_client.close()
-        redis_client = None
+    pool = ConnectionPool.from_url(
+        f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/{settings.REDIS_DB}",
+        encoding="utf-8",
+    )
+    client = Redis(connection_pool=pool)
+    try:
+        yield client
+    finally:
+        await client.close()
+        await pool.disconnect()
 
 
 async def add_token_to_blacklist(
     token: str, expiry_time_in_seconds: int = 1800
 ) -> None:
-    client = await get_redis_client()
-    await client.setex(token, expiry_time_in_seconds, "blacklisted")
+    async with get_redis_client() as client:
+        await client.setex(name=token, value="blacklisted", time=expiry_time_in_seconds)
 
 
 async def is_token_blacklisted(token: str) -> bool:
-    client = await get_redis_client()
-    return await client.exists(token) > 0
+    async with get_redis_client() as client:
+        return await client.exists(token) > 0
--- src/main.py
+++ src/main.py
@@ -1,25 +1,13 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 
 from auth.router import router as auth_router
 from log.logging_config import logger
-from src.auth.redis import close_redis_client, get_redis_client
 from user.router import router as user_router
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await get_redis_client()
-    yield
-    await close_redis_client()
-
 
 app = FastAPI(
     title="User Management service",
     description="Main service is responsible for registration \
                 and authentication users",
-    lifespan=lifespan,
 )
 
 
@@ -35,4 +23,4 @@ app.include_router(user_router)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
